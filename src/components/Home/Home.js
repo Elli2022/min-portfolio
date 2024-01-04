5e3361685ef549aaa7d7fe7d67487bd4af908316
Eleonora Nocentini Sköldebrink <eleonora.nocentini@gmail.com>
2024-01-04 16:44:08 +0100

    Home.js, Home.css

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Välkommen till Min Portfölj</h1>
+      <p>
+        Hej! Jag är Eleonora, en passionerad front-end utvecklare som strävar
+        efter att bli fullstack-utvecklare.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
