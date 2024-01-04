eadb554bd216401678612904813cbac2a2eef4a3
Eleonora Nocentini Sköldebrink <eleonora.nocentini@gmail.com>
2024-01-04 18:16:49 +0100

    Navbar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,9 +2,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <Link to="/" className="nav-link">
+        Hem
+      </Link>
+      <Link to="/about" className="nav-link">
+        Om Mig
+      </Link>
+      <Link to="/projects" className="nav-link">
+        Projekt
+      </Link>
+      <Link to="/contact" className="nav-link">
+        Kontakt
+      </Link>
+    </nav>
+  );
+}
+
 function Home() {
   return (
     <>
+      <Navbar />
       <div className="home">
         <h1>Välkommen till Min Portfölj</h1>
         <p>
